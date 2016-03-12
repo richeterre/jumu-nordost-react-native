8e588aa969a0ccdcf80c80636ccfcdc1c72b42b3
Martin Richter <me@martinrichter.net>
2016-03-12 14:01:03 +0200

    List appearances and pieces in performance detail screen

--- PerformanceScreen.js
+++ PerformanceScreen.js
@@ -13,11 +13,32 @@ class PerformanceScreen extends Component {
     const venue = this.props.venue
     return (
       <View style={styles.container}>
-        <Text>{performance.stage_time}</Text>
-        <Text>{venue.name}</Text>
-        <Text>{performance.category_name}</Text>
-        <Text>Altersgruppe {performance.age_group}</Text>
-        <Text>{performance.predecessor_host_name}</Text>
+        <View style={styles.generalInfo}>
+          <Text>{performance.stage_time}</Text>
+          <Text>{venue.name}</Text>
+          <Text>{performance.category_name}</Text>
+          <Text>Altersgruppe {performance.age_group}</Text>
+        </View>
+        <View style={styles.appearancesInfo}>
+          {performance.appearances.map((appearance, i) =>
+            <Text key={'appearance' + (i + 1)}>
+              {appearance.participant_name}, {appearance.instrument_name}
+            </Text>
+          )}
+          <Text>{performance.predecessor_host_name}</Text>
+        </View>
+        <View style={styles.piecesInfo}>
+          {performance.pieces.map((piece, i) =>
+            <View key={'piece' + (i + 1)}>
+            <Text>
+              {piece.composer_name}
+            </Text>
+            <Text>
+              {piece.title}
+            </Text>
+            </View>
+          )}
+        </View>
       </View>
     )
   }
@@ -27,9 +48,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  generalInfo: {
+    paddingTop: 20,
+    paddingBottom: 20
+  },
+  appearancesInfo: {
+    paddingBottom: 20
   }
 });
 
