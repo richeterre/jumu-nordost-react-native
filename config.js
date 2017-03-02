e22298504d1b548bdb467e4275b5a5daebdbe1c1
Martin Richter <martin.richter@futurice.com>
2017-03-02 15:09:08 +0100

    Add local base URL for iOS

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ import secrets from './secrets' // See README
 
 export default {
   apiKey: secrets.apiKey,
+  // baseUrl: 'http://localhost:5000/api/v1/', // iOS,
   baseUrl: 'http://10.0.2.2:5000/api/v1/', // AVD,
   // baseUrl: 'http://192.168.56.1:5000/api/v1/' // Genymotion,
   // baseUrl: 'http://www.jumu-nordost.eu/api/v1/' // Production
